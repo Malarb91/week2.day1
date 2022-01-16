5d5c803e56970520bd33bd3d09fe7aa67d760155
Malarb91 <96664381+Malarb91@users.noreply.github.com>
2022-01-17 02:12:10 +0530

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,30 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("DemoCSR");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("crmsfa");
+driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Contacts")).click();
+		driver.findElement(By.linkText("Find Contacts")).click();
+driver.findElement(By.xpath("//span[text()='Email']")).click();
+driver.findElement(By.xpath("//input[@name='emailAddress']")).sendKeys("babu@testleaf.com");
+driver.findElement(By.xpath("//button[text()='Find Contacts']")).click();
+
+		
+		
+
+	}
+
+}
